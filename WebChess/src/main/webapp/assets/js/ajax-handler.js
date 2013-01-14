1c73173bf279b02fcbd20ef9c500d767c46a4396
piotrek <>
2013-01-14 23:47:02 +0100

    Bulk commit. Beware!

--- WebChess/src/main/webapp/assets/js/ajax-handler.js
+++ WebChess/src/main/webapp/assets/js/ajax-handler.js
@@ -1,31 +1,65 @@
+// Variables
 var jq = jQuery.noConflict();
-Window.alert("Hello js!");
+var firstTime = true;
+var buttonInsideColumn = "<td>" + "<button class='btn btn-small' type='button'>take a sit</button>" + "</td>";
+// Functions
 function refresh() {
+    // Avoid instant reloading page dude!
     jq(function() {
-        jq.post("/WebChess/",
-        {},
-            function(data){
-                // data contains the result list
-                var listOfMatches=" ";
-                jQuery.each(data, function(i, v){
-                    var match = data[i]; 
-                    listOfMatches=listOfMatches+"<tr>";
-                    listOfMatches=listOfMatches+"<td>"+match.matchId+"</td>";
-                    listOfMatches=listOfMatches+"<td>"+match.progress+"</td>";
-                    listOfMatches=listOfMatches+"</tr>";
-                });
-                jQuery("#currentMatches").html(listOfMatches);
+        jq.get("/WebChess/allTables", function(data,status){
+            //data contains the result list
+            var listOfChessTables=" ";
+            jq.each(data, function(i, v){
+                var table = data[i]; 
+                listOfChessTables += "<tr>";
+                // Id and gameTime parameters should always be not null
+                listOfChessTables += "<td>"+table.tableId+"</td>";
+                // If sits are empty then add buttons instead of player names
+                if (table.wplayer == null) {
+                    listOfChessTables += buttonInsideColumn;
+                } else {
+                    listOfChessTables += "<td>"+table.wplayer+"</td>";
+                }
+                if (table.bplayer == null) {
+                    listOfChessTables += buttonInsideColumn;
+                } else {
+                    listOfChessTables += "<td>"+table.bplayer+"</td>";
+                }
+                listOfChessTables += "<td>"+table.gameTime+"</td>";
+                listOfChessTables += "</tr>";
             });
+            jq("#currentChessTables").html(listOfChessTables);
+        });
     });
+
+    jQuery("#timer").text(jQuery.now());
     setTimeout(function(){
         refresh()
         },5000);
 }
             
-function add() {
-    jq(function() {
-        jq.post("/WebChess/createTable",
-        {});
+function createNewTable() {
+    // send new request to the server
+    var request = jq.ajax({
+        url: "/WebChess/createTable",
+        type: "post"
+    });
+
+    // callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // response contains id of the newly created table
+        myWindow=window.open('','','width=700,height=500');
+        myWindow.document.write("<h2>New chess table with id:"+response+" created!</h2>");
+        myWindow.focus();
+    });
+
+    // callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // log the error to the console
+        console.error(
+            "The following error occured: "+
+            textStatus, errorThrown
+            );
     });
 }
 
